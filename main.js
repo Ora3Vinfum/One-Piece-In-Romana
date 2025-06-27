9038fb66d987ec4a9d6f907fd2504edb3794db84
Ora3Vinfum <134880567+Ora3Vinfum@users.noreply.github.com>
2025-06-27 14:04:25 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,90 @@
+function openvolum1() {
+    document.getElementById("Volumul1").classList.add("open-capitole")
+}
+function closevolum1() {
+    document.getElementById("Volumul1").classList.remove("open-capitole")
+}
+function openvolum2() {
+    document.getElementById("Volumul2").classList.add("open-capitole")
+}
+function closevolum2() {
+    document.getElementById("Volumul2").classList.remove("open-capitole")
+}
+function openvolum3() {
+    document.getElementById("Volumul3").classList.add("open-capitole")
+}
+function closevolum3() {
+    document.getElementById("Volumul3").classList.remove("open-capitole")
+}
+function openvolum4() {
+    document.getElementById("Volumul4").classList.add("open-capitole")
+}
+function closevolum4() {
+    document.getElementById("Volumul4").classList.remove("open-capitole")
+}
+function openvolum5() {
+    document.getElementById("Volumul5").classList.add("open-capitole")
+}
+function closevolum5() {
+    document.getElementById("Volumul5").classList.remove("open-capitole")
+}
+function openvolum6() {
+    document.getElementById("Volumul6").classList.add("open-capitole")
+}
+function closevolum6() {
+    document.getElementById("Volumul6").classList.remove("open-capitole")
+}
+function openvolum7() {
+    document.getElementById("Volumul7").classList.add("open-capitole")
+}
+function closevolum7() {
+    document.getElementById("Volumul7").classList.remove("open-capitole")
+}
+function openvolum8() {
+    document.getElementById("Volumul8").classList.add("open-capitole")
+}
+function closevolum8() {
+    document.getElementById("Volumul8").classList.remove("open-capitole")
+}
+function openvolum9() {
+    document.getElementById("Volumul9").classList.add("open-capitole")
+}
+function closevolum9() {
+    document.getElementById("Volumul9").classList.remove("open-capitole")
+}
+function openvolum10() {
+    document.getElementById("Volumul10").classList.add("open-capitole")
+}
+function closevolum10() {
+    document.getElementById("Volumul10").classList.remove("open-capitole")
+}
+function openvolum11() {
+    document.getElementById("Volumul11").classList.add("open-capitole")
+}
+function closevolum11() {
+    document.getElementById("Volumul11").classList.remove("open-capitole")
+}
+function openvolum12() {
+    document.getElementById("Volumul12").classList.add("open-capitole")
+}
+function closevolum12() {
+    document.getElementById("Volumul12").classList.remove("open-capitole")
+}
+function openvolum13() {
+    document.getElementById("Volumul13").classList.add("open-capitole")
+}
+function closevolum13() {
+    document.getElementById("Volumul13").classList.remove("open-capitole")
+}
+function openvolum14() {
+    document.getElementById("Volumul14").classList.add("open-capitole")
+}
+function closevolum14() {
+    document.getElementById("Volumul14").classList.remove("open-capitole")
+}
+function openvolum15() {
+    document.getElementById("Volumul15").classList.add("open-capitole")
+}
+function closevolum15() {
+    document.getElementById("Volumul15").classList.remove("open-capitole")
+}
